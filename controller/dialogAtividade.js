e729c203eefa73719eed86d8b7fa1554ea4ba850
ayrtonlima20 <ayrton.mondim@hotmail.com>
2016-05-15 23:07:00 -0300

    add dialog new activitie and the content of the dialog

---
+++ controller/dialogAtividade.js
@@ -0,0 +1,77 @@
+angular.module('demo').controller('TabsActCtrl', function ($scope, $window,  $uibModal, $log) {
+    $scope.tabs = [{ 
+        title:'Dynamic Title 1', 
+        content: {
+            nome:"asd",
+            participante:"aa",
+            prioridade:"asda",
+            estimativa:"sad",
+            descricao:"asda"
+        } 
+    },{ 
+        title:'Dynamic Title 2', 
+        content: {
+            nome:"bvb",
+            participante:"bvb",
+            prioridade:"bv",
+            estimativa:"bvb",
+            descricao:"bvbv"
+        } 
+    }];
+
+    $scope.addAtividade = function() {
+        $scope.tabs.push({ 
+            title:'Title'+ ($scope.tabs.length + 1),
+            content: {
+                nome:"",
+                participante:"",
+                prioridade:"",
+                estimativda:"",
+                descricao:""
+            } 
+        }); 
+    };
+
+    $scope.dialog = {};
+    $scope.selected = null;
+    $scope.animationsEnabled = true;
+
+    $scope.openDialogAtividade = function (size, item) {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'view/dialogAtividade.html',
+            controller: 'ModalAtividadeCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    // $scope.dialog.selected = $scope.models.selected;
+                    $scope.dialog.selected = item;
+                    $scope.dialog.dataAmazon = $scope.dataAmazon;
+                    return $scope.dialog;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.toggleAnimation = function () {
+        $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
+});
+
+angular.module('demo').controller('ModalAtividadeCtrl', function ($scope, $uibModalInstance, items) {
+    $scope.dialog = items;
+
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.dialog.selected.item);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
--- controller/fillSprint.js
+++ controller/fillSprint.js
@@ -1,9 +1,9 @@
-angular.module("demo").controller("FillSprintCtrl", function($scope, forecast, $uibModal, $log) {
+angular.module("demo").controller("FillSprintCtrl", function($scope, forecast) {
 	
 	$scope.table = {
 		selected:null,
 		historias:{
-			"productBacklog":[{
+			"ProductBacklog":[{
 				name:"História 1", 
 				endereco:"mudou",
 				telefone:"12346"
@@ -16,7 +16,7 @@ angular.module("demo").controller("FillSprintCtrl", function($scope, forecast, $
 				endereco:"endereçado",
 				telefone:"456"
 			}],
-			"sprintBacklog":[{
+			"SprintBacklog":[{
 				name:"História 4", 
 				endereco:"mudou",
 				telefone:"12346"
@@ -31,46 +31,4 @@ angular.module("demo").controller("FillSprintCtrl", function($scope, forecast, $
 			}]
 		}
 	};	
-	$scope.dialog = {};
-	$scope.selected = null;
-	$scope.animationsEnabled = true;
-
-	$scope.open = function (size, item) {
-		var modalInstance = $uibModal.open({
-			animation: true,
-			templateUrl: 'view/dialogAtividade.html',
-			controller: 'ModalAtividadeCtrl',
-			size: size,
-			resolve: {
-				items: function () {
-          			// $scope.dialog.selected = $scope.models.selected;
-          			$scope.dialog.selected = item;
-          			$scope.dialog.dataAmazon = $scope.dataAmazon;
-          			return $scope.dialog;
-          		}
-          	}
-		});
-
-		modalInstance.result.then(function (selectedItem) {
-			$scope.selected = selectedItem;
-		}, function () {
-			$log.info('Modal dismissed at: ' + new Date());
-		});
-	};
-
-	$scope.toggleAnimation = function () {
-		$scope.animationsEnabled = !$scope.animationsEnabled;
-	};
-});
-
-angular.module('demo').controller('ModalAtividadeCtrl', function ($scope, $uibModalInstance, items) {
-	$scope.dialog = items;
-
-	$scope.ok = function () {
-		$uibModalInstance.close($scope.dialog.selected.item);
-	};
-
-	$scope.cancel = function () {
-		$uibModalInstance.dismiss('cancel');
-	};
 });
